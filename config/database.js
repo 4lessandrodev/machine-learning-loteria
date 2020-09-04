3443a735304c800e837f9d7e06abd51a313c466b
Alessandro <alessandroadm@live.com>
2020-09-04 16:19:01 -0300

    feat(env): adding setup dotenv

--- config/database.js
+++ config/database.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
 module.exports = {
-    
+username:process.env.DB_USER,
+password:process.env.DB_PASS,
+database:process.env.DB_NAME,
+host:process.env.DB_HOST,
+dialect:"mysql"
 }
