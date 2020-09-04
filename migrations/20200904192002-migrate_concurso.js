bb3f50124b0f71e2e92d835af6315f201fa92645
Alessandro <alessandroadm@live.com>
2020-09-04 16:25:21 -0300

    feat(database): adding seeders and migrations

--- migrations/20200904192002-migrate_concurso.js
+++ migrations/20200904192002-migrate_concurso.js
@@ -1,104 +1,109 @@
-/* jshint indent: 2 */
+'use strict';
 
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('concursos', {
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('concursos', {
     'id': {
-      type: DataTypes.INTEGER(11),
+      type: Sequelize.INTEGER(11),
       allowNull: false,
       primaryKey: true,
       comment: "null",
       autoIncrement: true
     },
     'data': {
-      type: DataTypes.DATEONLY,
+      type: Sequelize.DATEONLY,
       allowNull: false,
       comment: "null"
     },
     'arrecadacao_total': {
-      type: DataTypes.DECIMAL,
+      type: Sequelize.DECIMAL,
       allowNull: false,
       defaultValue: '0.00',
       comment: "null"
     },
     'ganhadores_15_numeros': {
-      type: DataTypes.INTEGER(11),
+      type: Sequelize.INTEGER(11),
       allowNull: false,
       defaultValue: '0',
       comment: "null"
     },
     'ganhadores_14_numeros': {
-      type: DataTypes.INTEGER(11),
+      type: Sequelize.INTEGER(11),
       allowNull: false,
       defaultValue: '0',
       comment: "null"
     },
     'ganhadores_13_numeros': {
-      type: DataTypes.INTEGER(11),
+      type: Sequelize.INTEGER(11),
       allowNull: false,
       defaultValue: '0',
       comment: "null"
     },
     'ganhadores_12_numeros': {
-      type: DataTypes.INTEGER(11),
+      type: Sequelize.INTEGER(11),
       allowNull: false,
       defaultValue: '0',
       comment: "null"
     },
     'ganhadores_11_numeros': {
-      type: DataTypes.INTEGER(11),
+      type: Sequelize.INTEGER(11),
       allowNull: false,
       defaultValue: '0',
       comment: "null"
     },
     'valor_rateio_15_numeros': {
-      type: DataTypes.DECIMAL,
+      type: Sequelize.DECIMAL,
       allowNull: false,
       defaultValue: '0.00',
       comment: "null"
     },
     'valor_rateio_14_numeros': {
-      type: DataTypes.DECIMAL,
+      type: Sequelize.DECIMAL,
       allowNull: false,
       defaultValue: '0.00',
       comment: "null"
     },
     'valor_rateio_13_numeros': {
-      type: DataTypes.DECIMAL,
+      type: Sequelize.DECIMAL,
       allowNull: false,
       defaultValue: '0.00',
       comment: "null"
     },
     'valor_rateio_12_numeros': {
-      type: DataTypes.DECIMAL,
+      type: Sequelize.DECIMAL,
       allowNull: false,
       defaultValue: '0.00',
       comment: "null"
     },
     'valor_rateio_11_numeros': {
-      type: DataTypes.DECIMAL,
+      type: Sequelize.DECIMAL,
       allowNull: false,
       defaultValue: '0.00',
       comment: "null"
     },
     'estimativa_premio': {
-      type: DataTypes.DECIMAL,
+      type: Sequelize.DECIMAL,
       allowNull: false,
       defaultValue: '0.00',
       comment: "null"
     },
     'valor_acumulado_15_numeros': {
-      type: DataTypes.DECIMAL,
+      type: Sequelize.DECIMAL,
       allowNull: false,
       defaultValue: '0.00',
       comment: "null"
     },
     'valor_acumulado_especial': {
-      type: DataTypes.DECIMAL,
+      type: Sequelize.DECIMAL,
       allowNull: false,
       defaultValue: '0.00',
       comment: "null"
     }
-  }, {
-    tableName: 'concursos'
-  });
+      }
+        );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.dropTable('concursos');
+  }
 };
