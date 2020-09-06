d0a3171a20ac8bd877f54b09b0c13f125f3270e4
Alessandro <alessandroadm@live.com>
2020-09-05 21:57:10 -0300

    config

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 module.exports = {
-username:process.env.DB_USER,
-password:process.env.DB_PASS,
-database:process.env.DB_NAME,
-host:process.env.DB_HOST,
-dialect:"mysql"
-}
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+};
